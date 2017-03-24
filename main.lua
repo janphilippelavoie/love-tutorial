721a99f52778c801bcdffd2a0536f0672026bc9e
Jan-Philippe Lavoie <janphilippelavoie@gmail.com>
2017-03-23 23:08:36 -0400

    Added enemies

--- main.lua
+++ main.lua
@@ -3,25 +3,60 @@ debug = true
 canShoot = true
 canShootTimerMax = 0.5
 canShootTimer = 0
-bulletSpeed = 100
+bulletSpeed = 1000
 bullets = {}
+enemies = {}
+enemySpawnRate = 1
+enemySpawnTimer = 0
+enemySpeed = 100
 
 function love.load(arg)
   player = { x = 200, y = 710, speed = 350,  img = nil }
   player.img = love.graphics.newImage('assets/aircrafts/Aircraft_01.png')
   bulletImage = love.graphics.newImage('assets/aircrafts/bullet_2_blue.png')
+  enemyImage = love.graphics.newImage('assets/aircrafts/Aircraft_07.png')
 end
 
 function love.update(dt)
-  controls(dt)
   moveBullets(dt)
   checkCanShoot(dt)
+  controls(dt)
+  spawnEnemies(dt)
+  moveEnemies(dt)
 end
 
 function love.draw(dt)
   love.graphics.draw(player.img, player.x, player.y)
   for i, bullet in ipairs(bullets) do
     love.graphics.draw(bullet.img, bullet.x, bullet.y)
+  end
+  for i, enemy in ipairs(enemies) do
+    love.graphics.draw(enemy.img, enemy.x, enemy.y, 0, 1, -1)
+  end
+end
+
+function spawnEnemies(dt)
+  if enemySpawnTimer < 0 then
+    enemy = {
+      x = love.math.random(love.graphics.getWidth() - enemyImage:getWidth()),
+      y = 0,
+      img = enemyImage
+    }
+    print(enemy.x)
+    table.insert(enemies, enemy)
+    enemySpawnTimer = enemySpawnRate
+  else
+    enemySpawnTimer = enemySpawnTimer - dt
+  end
+end
+
+function moveEnemies(dt)
+  for i, enemy in ipairs(enemies) do
+    if enemy.y > love.graphics.getHeight() then
+      table.remove(enemies, i)
+    else
+      enemy.y = enemy.y + (enemySpeed * dt)
+    end
   end
 end
 
@@ -79,4 +114,11 @@ function shoot()
     canShoot = false
     canShootTimer = canShootTimerMax
   end
+end
+
+function checkCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
 end
